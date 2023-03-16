fc17d6b58f1a17cfd01ef0e34a91e03bd5ea6dc6
mohammadreza karimi <mrkarimi9425@gmail.com>
2023-03-17 02:19:35 +0330

    config application

---
+++ app/server.js
@@ -0,0 +1,57 @@
+module.exports = class Application {
+    #express = require("express");
+    #app = this.#express();
+    constructor(PORT, DB_URL) {
+        this.configDatabase(DB_URL);
+        this.configApplication();
+        this.createServer(PORT);
+        this.createRoutes();
+        this.errorHandler();
+    }
+    configApplication() {
+        const path = require("path");
+        this.#app.use(this.#express.json());
+        this.#app.use(this.#express.urlencoded({ extended: true }));
+        this.#app.use(this.#express.static(path.join(__dirname, "..", "public")));
+    }
+    createServer(PORT) {
+        const http = require("http");
+        const server = http.createServer(this.#app);
+        server.listen(PORT, () => {
+            console.log(`Server Run > on port http://localhost:${PORT}`)
+        })
+    }
+    configDatabase(DB_URL) {
+        const mongoose = require("mongoose");
+        mongoose.connect(DB_URL)
+            .then(() => {
+                console.log("Connect to DB successfull...");
+            })
+            .catch(error => {
+                throw error;
+            })
+    }
+    errorHandler() {
+        this.#app.use((req, res, next) => {
+            return res.status(404).json({
+                status: 404,
+                message: "آدرس موردنظر یافت نشد"
+            })
+        })
+        this.#app.use((error, res, req, next) => {
+            const status = error?.status || error?.statusCode || 500;
+            const message = error?.message || "InternalServerError";
+            res.status(status).json({
+                status,
+                message
+            })
+        })
+    }
+    createRoutes() {
+        this.#app.get("/", (req, res) => {
+            res.json({
+                message: "this is my first application with node js"
+            })
+        })
+    }
+}
